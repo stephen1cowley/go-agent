17dafd8b00c3709267335e3bf42d40348c30c01c
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-07-28 13:00:15 +0100

    Chat bot with a tool call

--- chat_bot/chat_bot.go
+++ chat_bot/chat_bot.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -10,6 +11,7 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/sashabaranov/go-openai"
+	"github.com/sashabaranov/go-openai/jsonschema"
 )
 
 func main() {
@@ -19,6 +21,37 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Conversation")
 	fmt.Println("---------------------")
+
+	jsonSchema := jsonschema.Definition{
+		Type: jsonschema.Object,
+		Properties: map[string]jsonschema.Definition{
+			"num1": {
+				Type:        jsonschema.Integer,
+				Description: "The first number",
+			},
+			"num2": {
+				Type:        jsonschema.Integer,
+				Description: "The second number",
+			},
+			"num3": {
+				Type:        jsonschema.Integer,
+				Description: "The third number",
+			},
+		},
+	}
+
+	myFuncDef := openai.FunctionDefinition{
+		Name:        "number_multiplier",
+		Description: "Multiplies the three numbers together",
+		Parameters:  &jsonSchema,
+	}
+
+	myTool := openai.Tool{
+		Type:     openai.ToolType("function"),
+		Function: &myFuncDef,
+	}
+
+	myTools := []openai.Tool{myTool}
 
 	for {
 		fmt.Print("-> ")
@@ -35,8 +68,10 @@ func main() {
 		resp, err := client.CreateChatCompletion(
 			ctx,
 			openai.ChatCompletionRequest{
-				Model:    openai.GPT3Dot5Turbo,
-				Messages: messages,
+				Model:      openai.GPT3Dot5Turbo,
+				Messages:   messages,
+				Tools:      myTools,
+				ToolChoice: "required",
 			},
 		)
 
@@ -47,7 +82,16 @@ func main() {
 			continue
 		}
 
+		jsonData, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(string(jsonData))
 		content := resp.Choices[0].Message.Content
+		toolCall := resp.Choices[0].Message.ToolCalls[0].Function.Arguments
+		fmt.Println(toolCall)
 		messages = append(messages, openai.ChatCompletionMessage{
 			Role:    openai.ChatMessageRoleAssistant,
 			Content: content,
