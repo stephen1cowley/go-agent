a8f375de52e6774c10545b6797a69699477d9ba7
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-08-11 10:28:34 +0100

    Deleted not-needed files

--- shell_script/runServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to the website directory
-cd ~/my_website
-
-# Run the HTTP server on port 8000
-python3 -m http.server 8000 --bind 0.0.0.0
-
-echo "Server is running on http://<your-public-ip>:8000"
